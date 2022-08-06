02253ec6a7b5faccc05e4afc44db85e083bc6944
giedriusstankauskas <46478940+giedriusstankauskas@users.noreply.github.com>
2022-08-06 19:39:08 +0300

    add phrases and nltk_utils

---
+++ nltk_utils.py
@@ -0,0 +1,24 @@
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+# # only use this line for first installation
+# nltk.download('punkt')
+
+stemmer = PorterStemmer()
+
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
+
+words = ["organize", "Organizes", "organizing"]
+stemmed_words = [stem(w) for w in words]
+print(stemmed_words)
