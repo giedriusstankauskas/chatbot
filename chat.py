cc354d0d1d907a642d12689a57011ea0d6bc62cb
giedriusstankauskas <46478940+giedriusstankauskas@users.noreply.github.com>
2022-08-06 22:17:35 +0300

    complete chat.py bot is working

--- chat.py
+++ chat.py
@@ -5,5 +5,44 @@ from model import SNet
 from nltk_utils import tokenize, bag_of_words
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = SNet(input_size, hidden_size, output_size).to(device)
 
+with open("intents.json", "r") as f:
+    intents = json.load(f)
+
+data = torch.load("data.pth")
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+model_state = data["model_state"]
+all_words = data["all_words"]
+tags = data["tags"]
+
+model = SNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Barista"
+print("Let's chat! Type 'quit' ti exit.")
+
+while True:
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent['tag']:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: Sorry, I don't understand. Please ask another question.")
