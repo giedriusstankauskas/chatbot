89d17f928b2c6eecef8cd29958a713654c30f281
giedriusstankauskas <46478940+giedriusstankauskas@users.noreply.github.com>
2022-08-06 23:53:49 +0300

    add front end

--- chat.py
+++ chat.py
@@ -22,13 +22,9 @@ model.load_state_dict(model_state)
 model.eval()
 
 bot_name = "Barista"
-print("Let's chat! Type 'quit' ti exit.")
 
-while True:
-    sentence = input("You: ")
-    if sentence == "quit":
-        break
-    sentence = tokenize(sentence)
+def get_response(msg):
+    sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X)
@@ -43,6 +39,17 @@ while True:
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent['tag']:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: Sorry, I don't understand. Please ask another question.")
+                return random.choice(intent['responses'])
+    return "Sorry, I don't understand. Please ask another question."
+
+
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        print(resp)
