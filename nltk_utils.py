b3e533f5c9cea61b2bbe2fb62635c77bdb7f25f1
giedriusstankauskas <46478940+giedriusstankauskas@users.noreply.github.com>
2022-08-06 21:41:26 +0300

    add train.py and create model

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 
 # # only use this line for first installation
 # nltk.download('punkt')
@@ -16,9 +17,17 @@ def stem(word):
 
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
-
-
-words = ["organize", "Organizes", "organizing"]
-stemmed_words = [stem(w) for w in words]
-print(stemmed_words)
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
